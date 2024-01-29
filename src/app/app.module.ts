465c750d59ab3f1897e7365e3a9220e918402e26
prasanna <prasa_100@outlook.com>
2024-01-29 09:23:41 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormControlDirective } from './directives/form-control.directive';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { FormControlDirectiveComponent } from './form-control-directive/form-control-directive.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormControlDirective ,
+    FormControlDirectiveComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule ,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
